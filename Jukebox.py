ec169ee5b12e21b902aa8757d0a3cc946a70f773
BrianKellyCS <bkelly8946@gmail.com>
2024-03-30 14:38:54 -0700

    update color test

--- Jukebox.py
+++ Jukebox.py
@@ -5,12 +5,13 @@ import os
 
 class Color:
     RESET = '\033[0m'
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    YELLOW = '\033[33m'
-    BLUE = '\033[34m'
-    MAGENTA = '\033[35m'
-    CYAN = '\033[36m'
+    RED = '\033[1;31m'    # Brighter Red
+    GREEN = '\033[1;32m'  # Neon Green
+    YELLOW = '\033[1;33m' # Brighter Yellow
+    BLUE = '\033[1;34m'
+    MAGENTA = '\033[1;35m'
+    CYAN = '\033[1;36m'
+
 
 
 class Jukebox(object):
