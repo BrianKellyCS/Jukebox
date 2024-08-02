ae9fcdccf13cfca4e2e1e198c0833240b0d72be3
Brian <bkelly8946@gmail.com>
2024-08-01 23:20:19 -0700

    update to save config in db

--- src/config.py
+++ src/config.py
@@ -1,27 +1,64 @@
-import json
+import sqlite3
 
 class ConfigManager:
-    def __init__(self, config_path='config.json'):
-        self.config_path = config_path
+    def __init__(self, db_path='jukebox.db'):
+        self.db_path = db_path
+        self.init_config_table()
         self.config = self.load_config()
 
+    def init_config_table(self):
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+        cursor.execute('''
+        CREATE TABLE IF NOT EXISTS config (
+            key TEXT PRIMARY KEY,
+            value TEXT NOT NULL
+        )
+        ''')
+        default_config = {
+            'user_name': 'user',
+            'music_path': 'Music/',
+            'movies_path': 'Movies/',
+            'playlists_path': 'Playlists/'
+        }
+        for key, value in default_config.items():
+            cursor.execute('INSERT OR IGNORE INTO config (key, value) VALUES (?, ?)', (key, value))
+        conn.commit()
+        conn.close()
+
     def load_config(self):
-        with open(self.config_path, 'r') as config_file:
-            return json.load(config_file)
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+        cursor.execute('SELECT key, value FROM config')
+        config = {row[0]: row[1] for row in cursor.fetchall()}
+        conn.close()
+        return config
 
     def save_config(self):
-        with open(self.config_path, 'w') as config_file:
-            json.dump(self.config, config_file, indent=4)
+        conn = sqlite3.connect(self.db_path)
+        cursor = conn.cursor()
+        for key, value in self.config.items():
+            cursor.execute('REPLACE INTO config (key, value) VALUES (?, ?)', (key, value))
+        conn.commit()
+        conn.close()
         print("Configuration saved.")
 
-    def update_username(self, user_name):
+    def update_username(self, current_user_name):
         new_user_name = input("Enter your new user name: ").strip()
         if new_user_name:  # Check if the user actually entered something
-            user_name = new_user_name
             self.config['user_name'] = new_user_name
             self.save_config()  # Save the updated configuration
             print(f"Username updated to {new_user_name}.")
-            
+            return new_user_name
         else:
             print("Username update cancelled.")
-        return user_name
+            return current_user_name
+
+    def update_directory(self, directory_key):
+        new_path = input(f"Enter the new path for {directory_key.replace('_', ' ')}: ").strip()
+        if new_path:  # Check if the user actually entered something
+            self.config[directory_key] = new_path
+            self.save_config()  # Save the updated configuration
+            print(f"{directory_key.replace('_', ' ')} updated to {new_path}.")
+        else:
+            print(f"{directory_key.replace('_', ' ')} update cancelled.")
--- src/jukebox.py
+++ src/jukebox.py
@@ -8,31 +8,44 @@ from src.config import ConfigManager
 class Jukebox(object):
     def __init__(self):
         self.config_manager = ConfigManager()
-        self.youtube_manager = YouTubeManager(self.config_manager.config['directories']['music_path'],
-                                              self.config_manager.config['directories']['movies_path'])
+        self.init_paths()
+        self.youtube_manager = YouTubeManager(self.music_path, self.movies_path)
         self.media_explorer = MediaExplorer(
-            db_path = 'jukebox.db',
-            music_path=self.config_manager.config['directories']['music_path'],
-            movies_path=self.config_manager.config['directories']['movies_path'],
-            playlists_path=self.config_manager.config['directories']['playlists_path']
+            db_path='jukebox.db',
+            music_path=self.music_path,
+            movies_path=self.movies_path,
+            playlists_path=self.playlists_path
         )
         self.user_name = self.config_manager.config['user_name']
         self.Radio = 'https://media-ice.musicradio.com/Heart80s'
         self.from_youtube = False
-        self.currentMediaType = "Music"   #will be either Music or Video
+        self.currentMediaType = "Music"  # will be either Music or Video
         self.current_media = -1
-        self.videoType = 'Youtube' #will be Youtube or Movie
+        self.videoType = 'Youtube'  # will be Youtube or Movie
         self.downloadMovie = False
         self.checkForPlaylist = False
         self.welcome_screen = welcome_screen
         self.colored_prompt = colored_prompt
         self.menu = menu
 
+    def init_paths(self):
+        self.music_path = self.config_manager.config['music_path']
+        self.movies_path = self.config_manager.config['movies_path']
+        self.playlists_path = self.config_manager.config['playlists_path']
 
+    def update_paths(self):
+        self.music_path = self.config_manager.config['music_path']
+        self.movies_path = self.config_manager.config['movies_path']
+        self.playlists_path = self.config_manager.config['playlists_path']
+        
+        # Update the paths in media_explorer as well
+        self.media_explorer.music_path = self.music_path
+        self.media_explorer.movies_path = self.movies_path
+        self.media_explorer.playlists_path = self.playlists_path
 
     def start(self):
         query = ''
-        self.current_media = -1 #reset current media type
+        self.current_media = -1  # reset current media type
         self.welcome_screen(self.user_name)
         while query != 'q':
             self.current_media = -1
@@ -45,6 +58,8 @@ class Jukebox(object):
                 self.current_media = self.Radio
             if query == 'u':
                 self.user_name = self.config_manager.update_username(self.user_name)
+            if query == 'd':
+                self.update_directory()
             if query == 's':
                 self.media_explorer.index_all()
             if query == 'e':
@@ -52,43 +67,41 @@ class Jukebox(object):
 
             elif 'youtube.com' in query:
                 if ' -a' in query:
-                    query = query.replace(' -a','')
+                    query = query.replace(' -a', '')
                     self.youtube_manager.download_song(query)
                 else:
                     self.youtube_manager.download_video(query)
 
             elif ' -a' in query:
                 self.currentMediaType = "Music"
-                query = query.replace(' -a','')
+                query = query.replace(' -a', '')
                 if ' -p' in query:
                     self.checkForPlaylist = True
-                    query = query.replace(' -p','')
+                    query = query.replace(' -p', '')
                 print(query)
                 self.current_media = self.search(query)
             elif ' -v' in query:
                 self.currentMediaType = "Video"
-                query = query.replace(' -v','')
+                query = query.replace(' -v', '')
                 if ' -p' in query:
                     self.checkForPlaylist = True
-                    query = query.replace(' -p','')
+                    query = query.replace(' -p', '')
                 if ' -m' in query:
-                    query = query.replace(' -m','')
+                    query = query.replace(' -m', '')
                     self.videoType = "Movie"
                     if ' -d' in query:
-                        query = query.replace(' -d','')
+                        query = query.replace(' -d', '')
                         self.downloadMovie = True
                 elif ' -yt' in query:
-                    query = query.replace(' -yt','')
-                    self.videoType = "Youtube"    
+                    query = query.replace(' -yt', '')
+                    self.videoType = "Youtube"
 
                 print(query)
                 self.current_media = self.search(query)
                 print("Current media set as: ", self.current_media)
 
-
-            elif self.current_media == -1 and query != 'q' and query != 'help' and query != 'u' and query != 's':
+            elif self.current_media == -1 and query != 'q' and query != 'help' and query != 'u' and query != 's' and query != 'd':
                 print('Specify if you want audio/video by adding -a or -v after your query (type "help" for more info)')
-                
 
             if self.current_media != -1:
                 self.media_explorer.play(self.current_media, self.currentMediaType, self.videoType, self.Radio)
@@ -100,21 +113,33 @@ class Jukebox(object):
                         self.youtube_manager.download(self.current_media, self.currentMediaType)
                         self.media_explorer.index_directory
 
-   
-   
-   
-    def search(self,query):
+    def update_directory(self):
+        print("Which directory would you like to update?")
+        print("1. Music Path")
+        print("2. Movies Path")
+        print("3. Playlists Path")
+        choice = input("Enter the number of your choice: ").strip()
+        if choice == '1':
+            self.config_manager.update_directory('music_path')
+        elif choice == '2':
+            self.config_manager.update_directory('movies_path')
+        elif choice == '3':
+            self.config_manager.update_directory('playlists_path')
+        else:
+            print("Invalid choice. Directory update cancelled.")
+        self.update_paths()
 
-        #Case 1: Check if media is already in playlist directory. 
-        media = self.media_explorer.search_db_directory(query, self.currentMediaType)# if self.search_db_directory(query) != -1 else ''
-        print("****",self.currentMediaType)
+    def search(self, query):
+        # Case 1: Check if media is already in playlist directory.
+        media = self.media_explorer.search_db_directory(query, self.currentMediaType)  # if self.search_db_directory(query) != -1 else ''
+        print("****", self.currentMediaType)
         if media:
             check_online = input(f"Found {media} in your local directory:\nWould you like to play this? (y/n)")
-            print("You entered: ",check_online)
+            print("You entered: ", check_online)
             if check_online == 'y':
                 return media
 
-        #Case 2: Search online for media
+        # Case 2: Search online for media
         media = -1
         if self.currentMediaType == 'Music':
             self.from_youtube, media = self.youtube_manager.search_youtube(query, self.checkForPlaylist)
@@ -129,13 +154,13 @@ class Jukebox(object):
                     os.system(f'mov-cli -s films {query}')
             elif self.videoType == 'Youtube':
                 self.from_youtube, media = self.youtube_manager.search_youtube(query, self.checkForPlaylist)
-            
-            #default as youtube search for now
+
+            # default as youtube search for now
             else:
                 self.from_youtube, media = self.youtube_manager.search_youtube(query, self.checkForPlaylist)
 
-
         return media
+
 
 
 
